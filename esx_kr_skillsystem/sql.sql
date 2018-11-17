4372c257e5055d7727e77c6bb976721bc6166012
KRILLE123 <42100456+KRILLE123@users.noreply.github.com>
2018-11-17 15:39:53 +0100

    Update sql.sql

--- esx_kr_skillsystem/sql.sql
+++ esx_kr_skillsystem/sql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `skill` ( 
-  `identifier` varchar(255) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) NOT NULL,
   `gym` varchar(255) NOT NULL,
   PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+)
