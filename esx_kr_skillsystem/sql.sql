e3b668c838404fb103cf47195573e83b45357307
KRILLE123 <42100456+KRILLE123@users.noreply.github.com>
2018-11-17 19:09:52 +0100

    Update sql.sql

--- esx_kr_skillsystem/sql.sql
+++ esx_kr_skillsystem/sql.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS `skill` (
   `gymstatus` varchar(255) NOT NULL,
   PRIMARY KEY (`identifier`)
 )
-COLLATE='utf8mb4_unicode_ci'
+COLLATE='latin_swedish_ci'
 ENGINE=InnoDB
 ;
